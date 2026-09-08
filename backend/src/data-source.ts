afe0de4fb6afcd1d906fdb77636c3022d4551bec
Arpit <arpityadav58571@gmail.com>
2026-09-08 21:40:23 +0530

    new

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -14,7 +14,7 @@ dotenv.config()
 export const AppDataSource = new DataSource({
     type: "mysql",
     host:process.env.DB_HOST,
-    port:3306,
+    port:28468,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
