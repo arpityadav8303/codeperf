46416b433fc034101bd82f192ee296127ee5ccc0
Arpit <arpityadav58571@gmail.com>
2026-09-08 22:24:53 +0530

    m

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -14,8 +14,8 @@ import { AddIsChangePassToUsers1781000000000 } from "./migrations/1781000000000-
 dotenv.config()
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host:process.env.DB_HOST,
-    port:3306,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
